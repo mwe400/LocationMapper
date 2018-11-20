89b0bc0b91b91980a39072b87955b03f480d10d3
mwe400 <matthew.weber@rutgers.edu>
2018-11-19 22:40:53 -0600

    Updated variables names

--- NMRP_gecodeAPI.R
+++ NMRP_gecodeAPI.R
@@ -11,46 +11,46 @@ library(mapview)
 library(sp)
 
 #load the csv file into the workspace
-geo1 <- read.csv(file.choose(), header = TRUE, sep =",")
+GeoData <- read.csv(file.choose(), header = TRUE, sep =",")
 
 #some data cleaning
-Matt1 <- geo1
-str(Matt1)
-Matt1 <- Matt1[, -1]
+DataSet <- GeoData
+str(DataSet)
+DataSet <- DataSet[, -1]
 
 #changing the data types
-Matt1$County <- as.character(Matt1$County)
-Matt1$Location <- as.character(Matt1$Location)
-str(Matt1)
-summary(Matt1)
+DataSet$County <- as.character(DataSet$County)
+DataSet$Location <- as.character(DataSet$Location)
+str(DataSet)
+summary(DataSet)
 
 #consolidating the records based on County
-dataMatt <- Matt1 %>%
+CompositeData <- DataSet %>%
             select(County,Location,Frequency) %>%
             group_by(County, Location) %>%
             summarise(Frequency = sum(Frequency))
 
-str(dataMatt)
+str(CompositeData)
 
 #create a new varaible for the count of words in the location variable
-dataMatt$word <-  vapply(strsplit(dataMatt$Location, "\\W+"), length, integer(1))
+CompositeData$word <-  vapply(strsplit(CompositeData$Location, "\\W+"), length, integer(1))
 
 #Omit the locations which are having the word count higher than 10
-dataMatt <- subset(dataMatt ,dataMatt$word < 6, select =  c(1:3))
+CompositeData <- subset(CompositeData ,CompositeData$word < 6, select =  c(1:3))
 
-geo1 <- dataMatt
+FinalData <- CompositeData
 
-View(geo1)
+View(FinalData)
 
 #create new columns to call the geocode function
-geo1$State <- NA
-geo1$lat<-NA
-geo1$long<-NA
-geo1$formatted_address<-NA
+FinalData$State <- NA
+FinalData$lat<-NA
+FinalData$long<-NA
+FinalData$formatted_address<-NA
 
 #Create a new column to split the Statename from the county
-geo1$State <- as.data.frame(str_split_fixed(as.character(geo1$County), "-", 2))[,2]
-geo1$State <- trimws(geo1$State)
+FinalData$State <- as.data.frame(str_split_fixed(as.character(geo1$County), "-", 2))[,2]
+FinalData$State <- trimws(geo1$State)
 
 #Defining geocode function with the API-KEY
 getGeoData <- function(location) {
@@ -63,33 +63,33 @@ getGeoData <- function(location) {
 
 #loop to generate the geographical coordinates of every record
 #Pick the first location coordinates in case of one value
-for(i in 1:nrow(geo1))
+for(i in 1:nrow(FinalData))
 {
-  jsonData <- getGeoData(as.character(geo1$Location[i]))
-  #jsonData <- geocode(as.character(geo1$Location[i]),output="all",override_limit=TRUE)
+  jsonData <- getGeoData(as.character(FinalData$Location[i]))
+  #jsonData <- geocode(as.character(FinalData$Location[i]),output="all",override_limit=TRUE)
  
   print(geo1$Location[i])
   print(jsonData$status)
   if (jsonData$status == "OK") {
     if (length(jsonData$results) == 1) {
        formattedAddress <- jsonData$results[[1]]$formatted_address
-       geo1$formatted_address[i] <- formattedAddress
+       FinalData$formatted_address[i] <- formattedAddress
        #Focusing only on the locations pertaining to USA
        if(grepl("USA", formattedAddress)) {
-         geo1$lat[i] <- getElement(jsonData$results[[1]]$geometry$location,"lat")
-         geo1$long[i] <- getElement(jsonData$results[[1]]$geometry$location,"lng")
+         FinalData$lat[i] <- getElement(jsonData$results[[1]]$geometry$location,"lat")
+         FinalData$long[i] <- getElement(jsonData$results[[1]]$geometry$location,"lng")
        }
        
        #If there are multiple locations, pick the location coordinates matching with its corresponding state
     } else {
       for(j in 1:length(jsonData$results)) {
         formattedAddress <- jsonData$results[[j]]$formatted_address
-        geo1$formatted_address[i] <- formattedAddress
+        FinalData$formatted_address[i] <- formattedAddress
         if(grepl("USA", formattedAddress)) {
           print(formattedAddress)
           if(grepl(paste(", ",as.character(geo1$State[i]),sep=""), formattedAddress)) {
-            geo1$lat[i] <- getElement(jsonData$results[[j]]$geometry$location,"lat")
-            geo1$long[i] <- getElement(jsonData$results[[j]]$geometry$location,"lng")
+            FinalData$lat[i] <- getElement(jsonData$results[[j]]$geometry$location,"lat")
+            FinalData$long[i] <- getElement(jsonData$results[[j]]$geometry$location,"lng")
           }
         }
         
@@ -101,7 +101,7 @@ for(i in 1:nrow(geo1))
 }
 
 #check if the latitude and longitudinal coordinates populated
-View(geo1)
+View(FinalData)
 
 #get the USA map
 USAMap = ggmap(get_map(location = geocode("United States"), scale= 1, zoom= 4,maptype = "roadmap"),  source = "google")
@@ -118,7 +118,7 @@ scale_size_continuous(range=range(geo1$Frequency)) +geom_text(data = geo1, aes(x
 #To Create a interactive map
 
 #omit the NA's records for geographical coordinates
-geo1spat <- na.omit(geo1)
+geo1spat <- na.omit(FinalData)
 
 #use sp functions and feed the coordinates
 coordinates(geo1spat) <- ~long+lat
